4d0aaefc1e6d47e452927bd52324ca054c2f0755
raihan <5421449+ra89fi@users.noreply.github.com>
2022-05-27 11:34:10 +0600

    reviews

--- src/components/shared/Reviews.js
+++ src/components/shared/Reviews.js
@@ -3,7 +3,33 @@ import React from 'react';
 const Reviews = () => {
     return (
         <div className="reviews">
-            <p>This is reviews</p>
+            <div className="review">
+                <img src="/images/user.jpg" alt="" />
+                <blockquote>
+                    Incidunt deleniti blanditiis quas aperiam recusandae
+                    consequatur ullam quibusdam cum libero illo rerum
+                    repellendus!
+                </blockquote>
+                <p>-Mary Jane</p>
+            </div>
+            <div className="review">
+                <img src="/images/user.jpg" alt="" />
+                <blockquote>
+                    Incidunt deleniti blanditiis quas aperiam recusandae
+                    consequatur ullam quibusdam cum libero illo rerum
+                    repellendus!
+                </blockquote>
+                <p>-Mary Jane</p>
+            </div>
+            <div className="review">
+                <img src="/images/user.jpg" alt="" />
+                <blockquote>
+                    Incidunt deleniti blanditiis quas aperiam recusandae
+                    consequatur ullam quibusdam cum libero illo rerum
+                    repellendus!
+                </blockquote>
+                <p>-Mary Jane</p>
+            </div>
         </div>
     );
 };
--- src/components/shared/Tools.js
+++ src/components/shared/Tools.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Tools = () => {
     return (
@@ -8,6 +9,9 @@ const Tools = () => {
                     <img src="/images/tools/Tool1.jpg" alt="" />
                     <div>
                         <h5>Name Tool1</h5>
+                        <Link to="/purchase" className="btn btn-secondary">
+                            Order Now
+                        </Link>
                     </div>
                 </div>
             ))}
