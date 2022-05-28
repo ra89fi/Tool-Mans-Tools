e30c943b76c327130074b5472f0040bd277b7468
raihan <5421449+ra89fi@users.noreply.github.com>
2022-05-28 06:20:19 +0600

    add review

--- src/components/shared/ReviewForm.js
+++ src/components/shared/ReviewForm.js
@@ -1,9 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 const ReviewForm = ({ user }) => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
     const onReviewSubmit = (e) => {
         e.preventDefault();
         e.stopPropagation();
+        formRef.current.submit.disabled = true;
+        setLoading(true);
+        const data = {
+            name: user.displayName || 'Anonymous',
+            review: formRef.current.review.value,
+            rating: formRef.current.rating.value,
+            image: user.photoURL || '',
+        };
+        fetch(`${process.env.REACT_APP_BACK_URL}/reviews`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((data) => data.json())
+            .then((result) => {
+                console.log(result);
+                if (result.message === 'ok') {
+                    e.target.reset();
+                }
+            })
+            .catch((err) => setError(err))
+            .finally(() => {
+                formRef.current.submit.disabled = false;
+                setLoading(false);
+            });
     };
     const formRef = useRef();
     return (
@@ -26,15 +55,17 @@ const ReviewForm = ({ user }) => {
                 />
                 <input
                     type="range"
-                    class="form-range"
+                    className="form-range"
                     min="0"
                     max="5"
                     step="0.5"
                     id="rating"
                 />
-                <button type="submit" className="btn btn-secondary">
+                <button type="submit" className="btn btn-secondary" id="submit">
                     Submit
                 </button>
+                {error && <p className="text-danger errTxt">{error.message}</p>}
+                {loading && <p>Loading...</p>}
             </form>
         </div>
     );
